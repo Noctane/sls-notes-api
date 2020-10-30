7ea1b6117c29ec25dcaa38c20ee85831bea56b12
Arnaud PRAT <arnaud.prat@morning.com>
2020-10-30 16:24:51 +0100

    ADD billing api

---
+++ billing.js
@@ -0,0 +1,23 @@
+import stripePackage from 'stripe';
+import handler from './libs/handler-lib';
+import { calculateCost } from './libs/billing-lib';
+
+export const main = handler(async (event, context) => {
+  // storage -> notes number to store
+  // source -> stripe token for the card to charge
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = 'Scratch charge';
+
+  // Load secret key
+  const stripe = stripePackage(process.env.STRIPE_SECRETE_KEY);
+
+  await stripe.charges.create({
+    source,
+    amount,
+    description,
+    currency: 'usd',
+  });
+
+  return { status: true };
+});
